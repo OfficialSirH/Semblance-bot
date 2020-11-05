b34b45eb74d7ba41b129cbf62ecb44287ec83080
OfficialSirH <micahbenac52@gmail.com>
2020-11-05 07:20:50 -0500

    Changes to the vote command UI

--- commands/vote.js
+++ commands/vote.js
@@ -15,9 +15,14 @@ module.exports.run = async (client, message, args) => {
 	.setTitle("Vote")
 	.setColor(randomColor())
 	.setThumbnail(client.user.avatarURL())
-	.setDescription("[Discord.boats](https://discord.boats/bot/668688939888148480)\n"+
-					"[Discordbotlist.com](https://discordbotlist.com/bots/semblance)\n"+
-					"[Top.gg](https://top.gg/bot/668688939888148480)")
+		.setDescription("**Websites that provide game boost/is interactible if voted on**",
+			["[Top.gg](https://top.gg/bot/668688939888148480)\n",
+				"**Extra voting sites/Don't have any type of interactivity yet**",
+				"[Discord.boats](https://discord.boats/bot/668688939888148480)",
+				"[Discordbotlist.com](https://discordbotlist.com/bots/semblance)",
+				"[Discord.bots.gg](https://discord.bots.gg/bots/668688939888148480)",
+				//"[Bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/668688939888148480)",
+			"[Botsfordiscord.com](https://botsfordiscord.com/bot/668688939888148480)"].join('\n'))
 	.setFooter(`Command called by ${message.author.tag}`, message.author.avatarURL());
 	message.channel.send(embed);
 }
