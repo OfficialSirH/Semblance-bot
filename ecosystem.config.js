db80a5cb395c2bcb3e36d30c71fbbd95d0ce3ab6
OfficialSirH <micahbenac52@gmail.com>
2020-11-12 16:52:19 -0500

    Connecting Semblance to PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps: [{
+        name: 'Semblance',
+        script: './bot.js',
+        watch: '.',
+        instances: 'max',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production"
+        }
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
