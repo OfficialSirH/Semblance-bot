a80d860ff30678ea454e656c59292342755f0641
SirH <micahbenac52@gmail.com>
2020-12-06 02:46:52 -0500

    Major improvements on `s!gametransfer`

--- commands/gametransfer.js
+++ commands/gametransfer.js
@@ -1,11 +1,11 @@
 ﻿const { MessageEmbed, MessageAttachment } = require('discord.js'),
 	randomColor = require("../constants/colorRandomizer.js"),
-	{ currentLogo } = require('../config.js');
-let pageOne = "https://i.imgur.com/BsjMAu6.png",
-	pageTwo = "https://i.imgur.com/QbDAOkF.png",
-	pageThree = "https://i.imgur.com/w1jEuzh.png",
-	pageFour = "https://i.imgur.com/6qTz2Li.png",
-	pageFive = "https://i.imgur.com/YNBHSw9.png";
+	{ currentLogo } = require('../config.js'),
+	pages = ["https://i.imgur.com/BsjMAu6.png",
+		"https://i.imgur.com/QbDAOkF.png",
+		"https://i.imgur.com/w1jEuzh.png",
+		"https://i.imgur.com/6qTz2Li.png",
+		"https://i.imgur.com/YNBHSw9.png"];
 
 module.exports = {
 	description: "See a step-by-step guide to transfering your game progress into the cloud and onto another device.",
@@ -21,126 +21,49 @@ module.exports.run = async (client, message, args) => {
 	let embed = new MessageEmbed()
 		.setTitle("Game Transfer")
 		.setColor(randomColor())
+		.setAuthor(message.author.tag, message.author.displayAvatarURL())
 		.attachFiles(currentLogo)
 		.setThumbnail("attachment://Current_Logo.png")
-		.setImage(pageOne)
-		.setDescription("First step:");
-	Promise.resolve(message.channel.send(embed))
-		.then(function (msg) {
-			msg.react("⬅️");
-			msg.react("➡️");
+		.setImage(pages[0])
+		.setDescription("Step 1:");
+	message.channel.send(embed)
+		.then(async (msg) => {
+			await msg.react("⬅️");
+			await msg.react("➡️");
 		});
 }
 
 async function TurnPage(client, reaction, user) {
-	let pages = [pageOne, pageTwo, pageThree, pageFour, pageFive];
-	if (reaction.message.embeds.length > 0) {
-		let embed = reaction.message.embeds[0];
-		if (embed.image && (embed.image.url == pageOne || embed.image.url == pageTwo || embed.image.url == pageThree || embed.image.url == pageFour || embed.image.url == pageFive)) {
-			let isPageOne = embed.image.url == pageOne;
-			let isPageTwo = embed.image.url == pageTwo;
-			let isPageThree = embed.image.url == pageThree;
-			let isPageFour = embed.image.url == pageFour;
-			let isPageFive = embed.image.url == pageFive;
-			if (reaction.emoji.name == "➡️") {
-				if (isPageOne) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageTwo)
-						.setDescription("Second step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageTwo) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageThree)
-						.setDescription("Third step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageThree) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageFour)
-						.setDescription("Fourth step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageFour) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageFive)
-						.setDescription("Final step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageFive) {
-					RemoveReaction(reaction, user);
-				}
-			}
-			if (reaction.emoji.name == "⬅️") {
-				if (isPageOne) {
-					RemoveReaction(reaction, user);
-				}
-				if (isPageTwo) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageOne)
-						.setDescription("First step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageThree) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageTwo)
-						.setDescription("Second step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageFour) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageThree)
-						.setDescription("Third step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-				if (isPageFive) {
-					embed = new MessageEmbed()
-						.setTitle("Game Transfer")
-						.setColor(randomColor())
-						.attachFiles(currentLogo)
-						.setThumbnail("attachment://Current_Logo.png")
-						.setImage(pageFour)
-						.setDescription("Fourth step:");
-					reaction.message.edit(new MessageEmbed(embed));
-					RemoveReaction(reaction, user);
-				}
-			}
-		}
+	let embed = reaction.message.embeds[0];
+	if (reaction.message.embeds.length == 0 || user.tag != embed.author.name || !embed.image || !pages.includes(embed.image.url)) return;
+
+	let currentPage = pages.indexOf(embed.image.url);
+
+	if (reaction.emoji.name == "➡️") {
+		currentPage = (currentPage == 4) ? 0 : ++currentPage;
+		embed = new MessageEmbed()
+			.setTitle("Game Transfer")
+			.setAuthor(user.tag, user.displayAvatarURL())
+			.setColor(randomColor())
+			.attachFiles(currentLogo)
+			.setThumbnail("atachment://Current_Logo.png")
+			.setImage(pages[currentPage])
+			.setDescription(`Step ${currentPage + 1}:`);
+		reaction.message.edit(embed);
+		RemoveReaction(reaction, user);
+
+	} else if (reaction.emoji.name == "⬅️") {
+		currentPage = (currentPage == 0) ? 4 : --currentPage;
+		embed = new MessageEmbed()
+			.setTitle("Game Transfer")
+			.setAuthor(user.tag, user.displayAvatarURL())
+			.setColor(randomColor())
+			.attachFiles(currentLogo)
+			.setThumbnail("atachment://Current_Logo.png")
+			.setImage(pages[currentPage])
+			.setDescription(`Step ${currentPage + 1}:`);
+		reaction.message.edit(embed);
+		RemoveReaction(reaction, user);
 	}
 }
 
