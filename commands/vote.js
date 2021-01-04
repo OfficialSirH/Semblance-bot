7f5e5b66826817c9a155db6399e326dc0bf343b0
SirH <micahbenac52@gmail.com>
2021-01-03 21:43:24 -0500

    Voting links updated(some may not work yet)

--- commands/vote.js
+++ commands/vote.js
@@ -16,13 +16,13 @@ module.exports.run = async (client, message, args) => {
 	.setColor(randomColor())
 	.setThumbnail(client.user.avatarURL())
 		.setDescription(["**Websites that provide game boost/is interactible if voted on**",
-				"[Top.gg](https://top.gg/bot/794033850665533450)\n",
+				`[Top.gg](https://top.gg/bot/${client.user.id})\n`,
 				"**Extra voting sites/Don't have any type of interactivity yet**",
-				"[Discord.boats](https://discord.boats/bot/668688939888148480)",
-				"[Discordbotlist.com](https://discordbotlist.com/bots/semblance)",
-				"[Discord.bots.gg](https://discord.bots.gg/bots/668688939888148480)",
-				//"[Bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/668688939888148480)",
-			"[Botsfordiscord.com](https://botsfordiscord.com/bot/668688939888148480)"].join('\n'))
-	.setFooter(`Command called by ${message.author.tag}`, message.author.avatarURL());
+				`[Discord.boats](https://discord.boats/bot/${client.user.id})`,
+				`[Discordbotlist.com](https://discordbotlist.com/bots/semblance)`,
+				`[Discord.bots.gg](https://discord.bots.gg/bots/${client.user.id})`,
+				//`[Bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/${client.user.id})`,
+			`[Botsfordiscord.com](https://botsfordiscord.com/bot/${client.user.id})`].join('\n')) // Old Semblance ID: 668688939888148480
+	.setFooter(`Command called by ${message.author.tag}`, message.author.displayAvatarURL());
 	message.channel.send(embed);
 }
