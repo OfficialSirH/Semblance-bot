7f7c0e953d73a0e217f04d7e8c9a95cb9f1e6275
SirH <micahbenac52@gmail.com>
2021-01-29 14:15:47 -0500

    Fixed AFK slash command

--- bot.js
+++ bot.js
@@ -337,7 +337,7 @@ client.on('message', message => {
 		}
 	}
 
-	if (!message.content.startsWith(`${prefix}afk `)) removeAfk(client, message, message.author.id);
+	if (!message.content.startsWith(`${prefix}afk `) && (!message.content.startsWith('</afk:804778881461911623>') || message.application == null)) removeAfk(client, message, message.author.id);
 	//Cell to Singularity Exclusive Code
 	let chName = message.channel.name;
 	if (message.guild.id == c2sID || message.guild.id == sirhGuildID) for (const [key, value] of Object.entries(autoCommands)) autoCommands[key].run(client, message, parseArgs(message.content));
