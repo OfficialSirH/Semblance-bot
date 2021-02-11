e40fc63d72ccadb3488f758e6ce185c0413b305b
SirH <micahbenac52@gmail.com>
2021-02-11 00:09:40 -0500

    Several jump link changes: check `s!changelog`

--- auto_scripts/jump.js
+++ auto_scripts/jump.js
@@ -1,6 +1,4 @@
 const { MessageEmbed, Collection, Util } = require('discord.js'), { parseArgs } = require('../constants'),
-    limitedJump = new Collection(), discordDefault = "https://discordapp.com/channels/",
-    sweepJump = setInterval(() => limitedJump.sweep((item) => true, 30000)),
     JTModel = require('../models/Jump.js');
 
 module.exports = {
@@ -13,59 +11,44 @@ module.exports = {
 }
 
 module.exports.run = async (client, message, args, recursiveCount = 0) => {
-    /*if (limitedJump.has(message.id)) {
-        let theMsg = limitedJump.get(message.id);
-        if (limitedJump.get(message.id) == 2) {
-            return limitedJump.delete(message.id);
-        } else limitedJump.set(message.id, theMsg + 1);
-    } else {
-        const jumpHandler = await JTModel.findOne({ guild: message.guild.id });
-        if (!jumpHandler || !jumpHandler.active) return;
-        limitedJump.set(message.id, 1);
-    }*/
     const jumpHandler = await JTModel.findOne({ guild: message.guild.id });
     if (!jumpHandler || !jumpHandler.active || recursiveCount == 2) return;
     
     let content = args.join(" ");
     
     const link = /https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<guildID>@me|\d{17,19})?\/(?<channelID>\d{17,20})\/(?<messageID>\d{17,20})/.exec(content);
-    if (link == null) return;
+    if (link == null) return; 
     const { groups: { guildID, channelID, messageID } } = link;
+    
+    if (!client.guilds.cache.has(guildID) || guildID != message.guild.id) return;
+    let channel = client.guilds.cache.get(guildID).channels.cache.get(channelID);
+    if (channel.nsfw) return; 
 
-    client.guilds.fetch(guildID).then(guild => {
-        let channel = guild.channels.cache.get(channelID);
-        if (channel.nsfw || guild.id != message.guild.id) {
-            return limitedJump.delete(message.id);
+    channel.messages.fetch(messageID).then(async (msg) => {
+        let embed = new MessageEmbed()
+            .setAuthor(msg.author.username, msg.author.displayAvatarURL())
+            .setThumbnail(message.author.displayAvatarURL())
+            .setDescription(msg.content)
+            .addField('Jump', `[Go to message!](${msg.url})`)
+            .setFooter(`#${msg.channel.name} quoted by ${message.author.tag}`)
+            .setTimestamp(msg.createdTimestamp);
+        
+        if (msg.embeds[0]) {
+            let title = (msg.embeds[0].title) ? msg.embeds[0].title : 'no title',
+                description = (msg.embeds[0].description) ? msg.embeds[0].description : 'no description';
+            embed.addField(`Embed: ${title}`, `Description: ${description}`);
+            if (msg.embeds[0].image) embed.setImage(msg.embeds[0].image.url);
         }
-        channel.messages.fetch(messageID).then(async (msg) => {
-            let embed = new MessageEmbed()
-                .setAuthor(msg.author.username, msg.author.displayAvatarURL())
-                .setThumbnail(message.author.displayAvatarURL())
-                .setDescription(msg.content)
-                .addField('Jump', `[Go to message!](${msg.url})`)
-                .setFooter(`#${msg.channel.name} quoted by ${message.author.tag}`)
-                .setTimestamp(msg.createdTimestamp);
-            if (msg.embeds[0]) {
-                let title = (msg.embeds[0].title) ? msg.embeds[0].title : 'no title',
-                    description = (msg.embeds[0].description) ? msg.embeds[0].description : 'no description';
-                embed.addField(`Embed: ${title}`, `Description: ${description}`);
-                if (msg.embeds[0].image) embed.setImage(msg.embeds[0].image.url);
-            }
-            if (msg.attachments && !embed.image) embed.setImage(msg.attachments.map(a => a.url).filter((item, ind) => item)[0]);
 
-            message.channel.send(embed).then(async m => {
-                if (limitedJump.get(message.id) >= 2) await m.edit(Util.removeMentions(content), { embed: embed });
-                await message.delete().catch(err => err);
-            });
-        }).catch(err => console.log(err));
+        let attachmentLink = /https?:\/\/(?:canary\.|cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
+        if (!embed.image && (msg.attachments || attachmentLink != null))
+             embed.setImage(msg.attachments ? msg.attachments.map(a => a.url).filter((item, ind) => item)[0] : attachmentLink[0]);
+
     }).catch(err => console.log(err));
-content = content.replace(/https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?:@me|\d{17,19}\/)?\d{17,20}\/\d{17,20}/.exec(content)[0], ``);
-args = parseArgs(content);
-if (recursiveCount < 1)
-    module.exports.run(client, message, args, ++recursiveCount);
-/*if (limitedJump.get(message.id) >= 2) {
-    return limitedJump.delete(message.id);
-}
-else module.exports.run(client, message, args);*/
-}
 
+    content = content.replace(/https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?:@me|\d{17,19}\/)?\d{17,20}\/\d{17,20}/.exec(content)[0], ``);
+    args = parseArgs(content);
+    
+    if (recursiveCount < 1)
+        module.exports.run(client, message, args, ++recursiveCount);
+}
