aa64363b2d3a9f80d56d2f616802f2a2ba3fa7fe
SirH <micahbenac52@gmail.com>
2021-02-12 02:31:32 -0500

    Fixed jump link

--- auto_scripts/jump.js
+++ auto_scripts/jump.js
@@ -11,48 +11,46 @@ module.exports = {
 }
 
 module.exports.run = async (client, message, args, recursiveCount = 0) => {
-    return;
     const jumpHandler = await JTModel.findOne({ guild: message.guild.id });
-    if (!jumpHandler || !jumpHandler.active || recursiveCount == 2) return;
+    if (recursiveCount == 2) return message.delete();
+    if (!jumpHandler || !jumpHandler.active) return;
     
     let content = args.join(" ");
-    
+
     const link = /https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<guildID>@me|\d{17,19})?\/(?<channelID>\d{17,20})\/(?<messageID>\d{17,20})/.exec(content);
-    if (link == null) return; 
+    if (link == null) return recursiveCount > 0 ? message.delete() : undefined; 
     const { groups: { guildID, channelID, messageID } } = link;
-    
-    if (!client.guilds.cache.has(guildID) || guildID != message.guild.id) return;
-    let channel = client.guilds.cache.get(guildID).channels.cache.get(channelID);
-    if (!channel || channel.nsfw) return; 
 
-    try {
-        const msg = await channel.messages.fetch(messageID);
-    
-        let embed = new MessageEmbed()
-        .setAuthor(msg.author.username, msg.author.displayAvatarURL())
-        .setThumbnail(message.author.displayAvatarURL())
-        .setDescription(msg.content)
-        .addField('Jump', `[Go to message!](${msg.url})`)
-        .setFooter(`#${msg.channel.name} quoted by ${message.author.tag}`)
-        .setTimestamp(msg.createdTimestamp);
+    client.guilds.fetch(guildID).then(guild => {
+        let channel = guild.channels.cache.get(channelID);
+        if (channel.nsfw || guild.id != message.guild.id) return;
 
-        if (msg.embeds[0]) {
-            let title = (msg.embeds[0].title) ? msg.embeds[0].title : 'no title',
-                description = (msg.embeds[0].description) ? msg.embeds[0].description : 'no description';
-            embed.addField(`Embed: ${title}`, `Description: ${description}`);
-            if (msg.embeds[0].image) embed.setImage(msg.embeds[0].image.url);
-        }
-        
-        let attachmentLink = /https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
-        if (!embed.image && (msg.attachments || attachmentLink != null))
+        channel.messages.fetch(messageID).then(async (msg) => {
+            let embed = new MessageEmbed()
+                .setAuthor(msg.author.username, msg.author.displayAvatarURL())
+                .setThumbnail(message.author.displayAvatarURL())
+                .setDescription(msg.content)
+                .addField('Jump', `[Go to message!](${msg.url})`)
+                .setFooter(`#${msg.channel.name} quoted by ${message.author.tag}`)
+                .setTimestamp(msg.createdTimestamp);
+            if (msg.embeds[0]) {
+                let title = (msg.embeds[0].title) ? msg.embeds[0].title : 'no title',
+                    description = (msg.embeds[0].description) ? msg.embeds[0].description : 'no description';
+                embed.addField(`Embed: ${title}`, `Description: ${description}`);
+                if (msg.embeds[0].image) embed.setImage(msg.embeds[0].image.url);
+            }
+
+            let attachmentLink = /https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
+            if (!embed.image && (msg.attachments || attachmentLink != null))
             embed.setImage(msg.attachments ? msg.attachments.map(a => a.url).filter((item, ind) => item)[0] : attachmentLink[0]);
-    
-    } catch(e) { return console.log(`${message.author.tag}(${message.author.id}) cause the following error:\n${e}`) }
+            
+            message.channel.send(embed);
 
-    content = content.replace(/https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?:@me|\d{17,19}\/)?\d{17,20}\/\d{17,20}/.exec(content)[0], ``);
-    args = parseArgs(content);
-    
-    if (recursiveCount < 1)
-        module.exports.run(client, message, args, ++recursiveCount);
-    else message.delete();
+            content = content.replace(/https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?:@me|\d{17,19}\/)?\d{17,20}\/\d{17,20}/.exec(content)[0], ``);
+            args = parseArgs(content);
+
+            module.exports.run(client, message, args, ++recursiveCount);
+        
+        }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
 }
