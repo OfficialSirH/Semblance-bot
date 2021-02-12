e1ab561fd553560caa626e50137cab21fca4ee70
SirH <micahbenac52@gmail.com>
2021-02-12 16:26:16 -0500

    several fixes for jump link: check `s!changelog`

--- auto_scripts/jump.js
+++ auto_scripts/jump.js
@@ -26,6 +26,9 @@ module.exports.run = async (client, message, args, recursiveCount = 0) => {
         if (channel.nsfw || guild.id != message.guild.id) return;
 
         channel.messages.fetch(messageID).then(async (msg) => {
+            let attachmentLink = /https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
+            msg.content = msg.content.replace(/https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/\w*(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content)[0], ``);
+
             let embed = new MessageEmbed()
                 .setAuthor(msg.author.username, msg.author.displayAvatarURL())
                 .setThumbnail(message.author.displayAvatarURL())
@@ -33,16 +36,15 @@ module.exports.run = async (client, message, args, recursiveCount = 0) => {
                 .addField('Jump', `[Go to message!](${msg.url})`)
                 .setFooter(`#${msg.channel.name} quoted by ${message.author.tag}`)
                 .setTimestamp(msg.createdTimestamp);
-            if (msg.embeds[0]) {
+            if (msg.embeds[0] && attachmentLink == null) {
                 let title = (msg.embeds[0].title) ? msg.embeds[0].title : 'no title',
                     description = (msg.embeds[0].description) ? msg.embeds[0].description : 'no description';
                 embed.addField(`Embed: ${title}`, `Description: ${description}`);
                 if (msg.embeds[0].image) embed.setImage(msg.embeds[0].image.url);
             }
 
-            let attachmentLink = /https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
-            if (!embed.image && (msg.attachments || attachmentLink != null))
-            embed.setImage(msg.attachments ? msg.attachments.map(a => a.url).filter((item, ind) => item)[0] : attachmentLink[0]);
+            if (embed.image == null && (msg.attachments.size > 0 || attachmentLink != null))
+            embed.setImage(msg.attachments.size > 0 ? msg.attachments.map(a => a.url).filter((item, ind) => item)[0] : attachmentLink[0]);
             
             message.channel.send(embed);
 
