7c3c932d54d7059c91673fe1e3e8374f54516ada
SirH <micahbenac52@gmail.com>
2021-03-03 16:10:12 -0500

    More info added to `s!roles`

--- commands/roles.js
+++ commands/roles.js
@@ -20,7 +20,9 @@ module.exports.run = async (client, message, args) => {
             "**Beta Tester**: This role is gained when you've joined and sent proof of being part of the beta program for C2S to <#496430259114082304>.",
             "**Server Events**: This role can be obtained by typing `-rr` in <#706852533393686581>, which this role means you'll get pinged for events happening in the server.",
             "**Martian Council**: This role is ***unobtainable*** as it's a moderator role so please stop asking how to get this role.",
-            "***DISCLAIMER***: by *proof* for having the beta doesn't mean sending a screenshot of '(beta)' in the Google Play Store, about 99% of the time, that's not actually the beta. Please send a screenshot of your version number, which you can find right at the bottom of the Menu UI in-game, if you want the Beta Tester role. Also, double check announcements if beta was fully released before posting ***I have Beta!!***."].join('\n\n'))
+            ["***NOTICE***",
+            "**Beta Tester proof**: by *proof* for having the beta doesn't mean sending a screenshot of '(beta)' in the Google Play Store, about 99% of the time, that's not actually the beta. Please send a screenshot of your version number, which you can find right at the bottom of the Menu UI in-game, if you want the Beta Tester role. Also, double check announcements if beta was fully released before posting ***I have Beta!!***.",
+            '**Reality Expert screenshots**: When sending a screenshot of your stats page for the Reality Expert role, send the ***ENTIRE stats page***, cropping out parts of the stats page will have your screenshot be ignored.'].join('\n')].join('\n\n'))
         .setFooter("*Epic* roles.");
     message.channel.send(embed);
 }
