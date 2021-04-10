db9bfedde6dc7f8566f8bde2a708f25bcc02e910
SirH <micahbenac52@gmail.com>
2021-04-10 16:59:30 -0400

    8ball slash command now responds in embeds

--- slash_commands/8ball.js
+++ slash_commands/8ball.js
@@ -1,3 +1,6 @@
+const { MessageEmbed } = require('discord.js'),
+    {randomColor}  = require('../constants');
+
 module.exports = {
     permissionRequired: 0
 }
@@ -5,24 +8,30 @@ module.exports = {
 module.exports.run = async (client, interaction) => {
     if (!interaction.data.options[0]) return [{ content: "Ask any question and Semblance will answer." }];
     let randomizedChoice = Math.ceil(Math.random() * 20);
-    if (randomizedChoice == 1) return [{ content: 'It is certain' }];
-    if (randomizedChoice == 2) return [{ content: 'It is decidely so.' }];
-    if (randomizedChoice == 3) return [{ content: 'Without a doubt' }];
-    if (randomizedChoice == 4) return [{ content: 'Yes - definitely.' }];
-    if (randomizedChoice == 5) return [{ content: 'You may rely on it.' }];
-    if (randomizedChoice == 6) return [{ content: 'As I see it, yes.' }];
-    if (randomizedChoice == 7) return [{ content: 'Most likely.' }];
-    if (randomizedChoice == 8) return [{ content: 'Outlook good.' }];
-    if (randomizedChoice == 9) return [{ content: 'Yes.' }];
-    if (randomizedChoice == 10) return [{ content: 'Signs point to yes.' }];
-    if (randomizedChoice == 11) return [{ content: 'Reply hazy, try again.' }];
-    if (randomizedChoice == 12) return [{ content: 'Ask again later.' }];
-    if (randomizedChoice == 13) return [{ content: 'Better not tell you now.' }];
-    if (randomizedChoice == 14) return [{ content: 'Cannot predict now.' }];
-    if (randomizedChoice == 15) return [{ content: 'Concentrate and ask again.' }];
-    if (randomizedChoice == 16) return [{ content: 'Don\'t count on it.' }];
-    if (randomizedChoice == 17) return [{ content: 'My reply is no.' }];
-    if (randomizedChoice == 18) return [{ content: 'My sources say no.' }];
-    if (randomizedChoice == 19) return [{ content: 'Outlook not so good.' }];
-    if (randomizedChoice == 20) return [{ content: 'Very doubtful.' }];
+    const embed = new MessageEmbed()
+        .setTitle('8ball')
+        .setAuthor(interaction.member.user.tag, interaction.member.user.avatarURL)
+        .setColor(randomColor);
+    let description = `Question: ${interaction.data.options[0].value}\nAnswer: `;
+    if (randomizedChoice == 1) description +='It is certain';
+    if (randomizedChoice == 2) description +='It is decidely so.';
+    if (randomizedChoice == 3) description +='Without a doubt';
+    if (randomizedChoice == 4) description +='Yes - definitely.';
+    if (randomizedChoice == 5) description +='You may rely on it.';
+    if (randomizedChoice == 6) description +='As I see it, yes.';
+    if (randomizedChoice == 7) description +='Most likely.';
+    if (randomizedChoice == 8) description +='Outlook good.';
+    if (randomizedChoice == 9) description +='Yes.';
+    if (randomizedChoice == 10) description +='Signs point to yes.';
+    if (randomizedChoice == 11) description +='Reply hazy, try again.';
+    if (randomizedChoice == 12) description +='Ask again later.';
+    if (randomizedChoice == 13) description +='Better not tell you now.';
+    if (randomizedChoice == 14) description +='Cannot predict now.';
+    if (randomizedChoice == 15) description +='Concentrate and ask again.';
+    if (randomizedChoice == 16) description +='Don\'t count on it.';
+    if (randomizedChoice == 17) description +='My reply is no.';
+    if (randomizedChoice == 18) description +='My sources say no.';
+    if (randomizedChoice == 19) description +='Outlook not so good.';
+    if (randomizedChoice == 20) description +='Very doubtful.';
+    return [{ embeds: [embed.toJSON()] }];
 }
