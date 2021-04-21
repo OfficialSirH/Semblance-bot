8a801ef4be0485810e2c9327f4136dbd4a7cc06b
OfficialSirH <micahbenac52@gmail.com>
2021-04-21 13:16:53 -0400

    modifications that'll be finished later

--- commands/joinbeta.js
+++ commands/joinbeta.js
@@ -1,4 +1,5 @@
-const { MessageEmbed } = require('discord.js'), {randomColor} = require('../constants'), { currentLogo } = require('../config').default; 
+const { MessageEmbed } = require('discord.js'), {randomColor} = require('../constants'), { currentLogo } = require('../config').default,
+    { Information } = require('./edit.js');
 
 module.exports = {
     description: "Info on how to become a beta tester",
@@ -12,6 +13,7 @@ module.exports = {
 }
 
 module.exports.run = async (client, message, args) => {
+    let infoHandler = await Information.findOne({ infoType: 'beta' });
     let embed = new MessageEmbed()
         .setTitle('Steps to join beta')
         .setColor(randomColor)
@@ -23,7 +25,7 @@ module.exports.run = async (client, message, args) => {
                             `Please read the form completely before asking any doubts `,
                             `NOTE: - the beta versions of the game will have a lot of bugs and there is a possibility that you might lose your progress, Enter at your own will.`,
                             `- Cells Support is a person, not a place. You can DM him here => <@647198180911349770>`,
-                            `**REMINDER : DO NOT POST YOUR EMAIL IN THE PUBLIC CHAT**`].join('\n'))
+                            `**REMINDER : DO NOT POST YOUR EMAIL IN THE PUBLIC CHAT AND DON'T PING CELLS SUPPORT**`].join('\n'))
         .setFooter(`Called by ${message.author.tag}`);
     message.channel.send(embed);
 }
