3df3d680ed72f529f5ee60455ea2c3099e740545
SirH <micahbenac52@gmail.com>
2021-04-21 17:36:34 -0400

    Added a join beta blocker

--- commands/joinbeta.js
+++ commands/joinbeta.js
@@ -20,12 +20,14 @@ module.exports.run = async (client, message, args) => {
         .attachFiles(currentLogo)
         .setThumbnail("attachment://Current_Logo.png")
         .setAuthor(message.author.tag, message.author.displayAvatarURL())
-        .setDescription([`Steps to join the beta testing of CelltoSingularity can be found in the form.`,
+        .setFooter(`Called by ${message.author.tag}`);
+
+        if (infoHandler.info?.toLowerCase() == 'none') embed.setDescription("There's no beta currently so there's no beta joining available.");
+        else embed.setDescription([`Steps to join the beta testing of CelltoSingularity can be found in the form.`,
                             `[Beta Signup Form](https://docs.google.com/document/d/1Ov_XvSn2AN1Q0EVJLgCnxfVkzdn6EL72LH5bhDP-aSc/edit?usp=sharing)`,
                             `Please read the form completely before asking any doubts `,
                             `NOTE: - the beta versions of the game will have a lot of bugs and there is a possibility that you might lose your progress, Enter at your own will.`,
                             `- Cells Support is a person, not a place. You can DM him here => <@647198180911349770>`,
                             `**REMINDER : DO NOT POST YOUR EMAIL IN THE PUBLIC CHAT AND DON'T PING CELLS SUPPORT**`].join('\n'))
-        .setFooter(`Called by ${message.author.tag}`);
     message.channel.send(embed);
 }
