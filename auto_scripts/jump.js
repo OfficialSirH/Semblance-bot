358c2e023edb642abd1992dced45e8b688be516b
OfficialSirH <micahbenac52@gmail.com>
2021-04-27 13:17:46 -0400

    Added logging for jump content

--- auto_scripts/jump.js
+++ auto_scripts/jump.js
@@ -25,7 +25,7 @@ module.exports.run = async (client, message, args, recursiveCount = 0) => {
         let channel = guild.channels.cache.get(channelID);
         if (channel.nsfw || guild.id != message.guild.id) return;
 
-        if (recursiveCount == 0) message.content.replace(link, '').length == 0 ? undefined : message.channel.send(message.content.replace(link, ''));
+        if (recursiveCount == 0) console.log(`Message Content: ${message.content}\nContent: ${content}\nResult for MC: ${message.content.replace(link, '')}\nResult for C: ${content.replace(link, '')}`);//message.content.replace(link, '').length == 0 ? undefined : message.channel.send(message.content.replace(link, ''));
 
         channel.messages.fetch(messageID).then(async (msg) => {
             let attachmentLink = /https?:\/\/(?:cdn\.)?discord(?:app)?\.com\/attachments\/\d{17,19}\/\d{17,20}\/(?<name>\w*\W*)(?:\.png|\.jpg|\.jpeg|\.webp|\.gif)/i.exec(msg.content);
