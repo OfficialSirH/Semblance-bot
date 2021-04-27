67469b5a35f70ad40640a5854d453b1e1036d70c
SirH <micahbenac52@gmail.com>
2021-04-27 01:02:46 -0400

    New(unfinished) additions to credits

--- commands/credits.js
+++ commands/credits.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js'), {randomColor} = require('../constants');
+const { MessageEmbed } = require('discord.js'), {randomColor} = require('../constants'), { MessageComponent } = require('../structures');
 
 module.exports = {
 	description: "Lists everyone that has helped with the project of Semblance, including myself(SirH).",
@@ -11,7 +11,7 @@ module.exports = {
 }
 
 module.exports.run = async (client, message, args) => {
-	let embed = new MessageEmbed()
+	const embed = new MessageEmbed()
 		.setTitle("Credits")
 		.setColor(randomColor)
 		.setDescription("Special Thanks to Aditya for motivating me from the very beginning to work on this bot. " +
@@ -22,9 +22,26 @@ module.exports.run = async (client, message, args) => {
 		.addFields(
 			{ name: "Developer", value: "SirH" },
 			{ name: "Special Thanks and Organizer", value: "Aditya" },
-			{ name: "Artist", value: "**Semblance Artist:** cabiie\n**Semblance Beta Artist:** Lemon ([Lemon's Instagram page](https://www.instagram.com/creations_without_limtation/))" },
+			{ name: "Artist", value: ["**Semblance:** cabiie",
+				"**Semblance Beta:** Lemon ([Lemon's Instagram page](https://www.instagram.com/creations_without_limtation/))",
+				"**Semblance Revisioned Art:** StarLuckArt(preview soon:tm:) ([DeviantArt](https://www.deviantart.com/starluckart) and [Personal Site](https://bubblestheprotogen.wixsite.com/starluckart))"] },
 			{ name: "Silly dude who makes up funny ideas", value: "NerdGamer2848" },
 			{ name: "Early Testers", value: "Aditya, Parrot, Diza, 0NrD, and Aure" },
 		);
-	message.channel.send(embed);
+	const components = new MessageComponent()
+			.addButton({
+				label: 'Show StarLuckArt\'s Work',
+				style: MesageComponent.STYLES.PRIMARY,
+				custom_id: JSON.stringify({
+					id: message.author.id,
+					command: 'credits'
+				})
+			}).components;
+	client.api.channels[message.channel.id].messages.post({ data: {
+		embed,
+		components
+	}});
+}
+
+module.exports.pressedButton = async (interaction) => {
 }
