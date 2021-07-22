abd62436f38dee2f02f3c1233b9ef422af48ad52
SirH <micahbenac52@gmail.com>
2021-07-22 03:16:33 -0400

    Added new secret to secret list

--- src/commands/secret.ts
+++ src/commands/secret.ts
@@ -30,9 +30,11 @@ module.exports.run = async (client: Semblance, message: Message, args: string[])
 			"5. While your game camera is still focused on the paradise bird, wait till the bird flies near a small island with the darwin bust statue and click the island.",
 			"6. In the savannah section of the land garden, activate camera mode and point the camera to top down so then you can see the top of the mountain ledges, near the lions and elephants you'll find... *sniff*... Archie's bones :(",
 			"7. Between the savannah and the jungle section where the river splits them apart, you'll find the Amazonian Dolphin.",
+			`8. If you have your camera positioned at the pond with the ducks at an angle pointing towards the left side of the mountain, you'll see a **Tibetan Fox** on the other side of the waterfall from the Ibex.`,
+			`You can also see the fox on the right side of the mountain if you have your camera positioned behind the 2 humans.`,
 			"**Secrets of the speedruns:**",
-			"8. Reach Singularity within 5 minutes.",
-			"9. Reach Singularity within 120 seconds!"].join("\n"));
+			"9. Reach Singularity within 5 minutes.",
+			"10. Reach Singularity within 120 seconds!"].join("\n"));
 	message.author.send({ embeds:[embed] }).catch(() => message.reply('Something went wrong while trying to send the message, you likely have your DMs closed, preventing you from receiving the message'));
 }
 
