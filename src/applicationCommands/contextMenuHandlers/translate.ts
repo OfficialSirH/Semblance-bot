aeb1b5ffa938325b58ca7e62a3c3a47f0bea56fb
SirH <micahbenac52@gmail.com>
2021-09-29 08:30:14 -0400

    add: info for new roles

--- src/applicationCommands/contextMenuHandlers/translate.ts
+++ src/applicationCommands/contextMenuHandlers/translate.ts
@@ -3,7 +3,7 @@ import { fetchDeepL } from '@semblance/src/constants/commands';
 import { ContextMenuInteraction, Message } from 'discord.js';
 
 export const run = async (interaction: ContextMenuInteraction, { options }: ContextMenuHandlerOptions) => {
-    const message = options.getMessage('message', true) as Message,
+    const message = options.getMessage('message', true),
     translateData = await fetchDeepL({ text: message.content });
 
     return interaction.reply({ content: `Language: ${translateData.detected_source_language}\n\n${translateData.text}`, ephemeral: true });
--- src/commands/roles.ts
+++ src/commands/roles.ts
@@ -24,9 +24,7 @@ module.exports.run = async (client: Semblance, message: Message, args: string[])
             "**Beta Tester**: This role is gained when you've joined and sent proof of being part of the beta program for C2S to <#496430259114082304>.",
             "**Server Events**: This role can be obtained by pressing the button below, which this role means you'll get pinged for events happening in the server.",
             "**Martian Council**: This role is ***unobtainable*** as it's a moderator role so please stop asking how to get this role.",
-            ["***NOTICE***",
-            "**Beta Tester proof**: by *proof* for having the beta doesn't mean sending a screenshot of '(beta)' in the Google Play Store, about 99% of the time, that's not actually the beta. Please send a screenshot of your version number, which you can find right at the bottom of the Menu UI in-game, if you want the Beta Tester role. Also, double check announcements if beta was fully released before posting ***I have Beta!!***.",
-            '**Reality Expert screenshots**: When sending a screenshot of your stats page for the Reality Expert role, send the ***ENTIRE stats page***, cropping out parts of the stats page will have your screenshot be ignored.'].join('\n')].join('\n\n'))
+            `**All of the other new roles**: https://canary.discord.com/channels/488478892873744385/496430259114082304/892369818387365910`].join('\n\n'))
         .setFooter("*Epic* roles."),
         hasServerEvents = message.member.roles.cache.has(c2sRoles.serverEvents as Snowflake),
     components = [new MessageActionRow()
