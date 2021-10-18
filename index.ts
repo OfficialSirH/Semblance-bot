921bc095dcfcb388e0e9eeb1103559f07ee4d815
SirH <micahbenac52@gmail.com>
2021-10-18 01:22:51 -0400

    extreme experimentations in progress

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 (await import('dotenv')).config();
 (await import('source-map-support')).install();
-(await import('module-alias/register'));
+(await import('./module-alias')).init();
 (await import(`@semblance/config`)).config();
 // Semblance client
 import { Semblance } from '@semblance/structures';
