1428f7c15df3aeb868a26d9ed148b9a1f83cc6d8
SirH <micahbenac52@gmail.com>
2021-10-21 15:30:56 -0400

    some fixes for remindme autocomplete

--- src/applicationCommands/autocompleteHandlers/remindme.ts
+++ src/applicationCommands/autocompleteHandlers/remindme.ts
@@ -11,6 +11,7 @@ export async function run(interaction: AutocompleteInteraction, options: Command
         const responseOptions = ['m', 'h', 'd', 'w', 'mo'].map(o => ({ name: `${focusedOption}${o}`, value: `${focusedOption}${o}` }));
         return interaction.respond(responseOptions);
     }
+    
     const inputTypes = ['mo', 'w', 'd', 'h', 'm'];
     const timeAmount = timeInputRegex.exec(focusedOption);
     const { groups: { months = '0', weeks = '0', days = '0', hours = '0', minutes = '0' } } = timeAmount as TimeLengthsString;
@@ -18,9 +19,15 @@ export async function run(interaction: AutocompleteInteraction, options: Command
         return { type: inputTypes.at(i), value: parseInt(t) };
     }).filter(t => t.value > 0);
     if (timeValues.length === 0) return;
-    const { groups: { previousInputType, numInput } } = timeInputAutocompleteAssistantRegex.exec(focusedOption);
+
+    let groups = { previousInputType: '', numInput: '' };
+    if (timeInputAutocompleteAssistantRegex.exec(focusedOption)) 
+        groups = timeInputAutocompleteAssistantRegex.exec(focusedOption).groups as { previousInputType: string, numInput: string };
+    const { previousInputType, numInput } = groups;
+
     const finalValue = previousInputType && previousInputType != 'm' ? 
     { type: inputTypes.at(inputTypes.indexOf(previousInputType)+1), value: parseInt(numInput) } : timeValues.at(-1);
+
     const responseOptions: ApplicationCommandOptionChoice[] = [];
     for (let i = 0; i < clamp(99 - timeValues.at(-1).value, 0, 5) + 1; i++) {
         const values = timeValues.map((t, ind) => ind != timeValues.length-1 ? `${t.value}${t.type}` : '').join('') +
