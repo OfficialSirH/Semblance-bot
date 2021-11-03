af66685be8beaea786fc7834cf979e92aa1b5288
SirH <micahbenac52@gmail.com>
2021-11-03 11:14:20 -0400

    fix(link): only respond ephemerally

--- src/applicationCommands/slashCommands/link.ts
+++ src/applicationCommands/slashCommands/link.ts
@@ -11,9 +11,11 @@ export default {
     const token = createHmac('sha1', process.env.USERDATA_AUTH).update(playerId).update(playerToken).digest('hex');
     const dataAlreadyExists = await UserData.findOne({ token });
     if (dataAlreadyExists)
-      return interaction.reply(
-        'The provided data seems to already exist, which means this data is already linked to a discord account, if you feel this is false, please DM the owner(SirH).',
-      );
+      return interaction.reply({
+        content:
+          'The provided data seems to already exist, which means this data is already linked to a discord account, if you feel this is false, please DM the owner(SirH).',
+        ephemeral: true,
+      });
     const updatedUser = !!(await UserData.findOneAndUpdate(
       { discordId: user.id },
       { $set: { token, edited_timestamp: Date.now() } },
@@ -21,17 +23,22 @@ export default {
     ));
     if (updatedUser) {
       console.log(`${user.tag}(${user.id}) successfully linked their C2S data.`);
-      return interaction.reply(
-        'The link was successful, now you can use the Discord button in-game to upload your progress.',
-      );
+      return interaction.reply({
+        content: 'The link was successful, now you can use the Discord button in-game to upload your progress.',
+        ephemeral: true,
+      });
     }
     const newUser = new UserData({ token, discordId: user.id });
     newUser.save(function (err) {
       if (err)
-        return interaction.reply(
-          "An error occured, either you provided incorrect input or something randomly didn't want to work.",
-        );
-      interaction.reply('The link was successful, now you can use the Discord button in-game to upload your progress.');
+        return interaction.reply({
+          content: "An error occured, either you provided incorrect input or something randomly didn't want to work.",
+          ephemeral: true,
+        });
+      interaction.reply({
+        content: 'The link was successful, now you can use the Discord button in-game to upload your progress.',
+        ephemeral: true,
+      });
     });
   },
 } as SlashCommand;
