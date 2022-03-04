653a3d3b706a1549f788cb63992948c9d786f088
SirH <micahbenac52@gmail.com>
2022-03-03 21:12:33 -0500

    rewrite secret category

--- src/commands/secret/mementomori.ts
+++ src/commands/secret/mementomori.ts
@@ -1,33 +1,26 @@
 import { Embed } from 'discord.js';
 import type { Message } from 'discord.js';
-import { randomColor } from '#constants/index';
+import { Categories, randomColor } from '#constants/index';
 import { mementoMori } from '#config';
 import { Command } from '@sapphire/framework';
 
-export default {
-  description: 'Memento Mori',
-  category: 'secret',
-  aliases: ['memento', 'unus', 'unusannus'],
-  permissionRequired: 0,
-  checkArgs: () => true,
-  run: (_client, message, args, identifier) => run(message, args, identifier),
-} as Command<'secret'>;
+export default class Mementomori extends Command {
+  public override name = 'mementomori';
+  public override fullCategory = [Categories.secret];
 
-const run = async (message: Message, args: string[], identifier: string) => {
-  if (identifier == 'mementomori' || identifier == 'unusannus') return sendIt(message);
-  if ((identifier == 'memento' && args[0] == 'mori') || (identifier == 'unus' && args[0] == 'mori'))
-    return sendIt(message);
-};
+  public override async sharedRun(builder: Command['SharedBuilder']) {
+    const user = 'user' in builder ? builder.user : builder.author;
+    const embed = new Embed()
+      .setTitle('Memento Mori')
+      .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+      .setColor(randomColor)
+      .setImage(mementoMori.name)
+      .setDescription('[The Goodbye](https://www.youtube.com/watch?v=aDQ3nfBbPWM)');
+    return { embeds: [embed], files: [mementoMori], ephemeral: true };
+  }
 
-async function sendIt(message: Message) {
-  const embed = new Embed()
-    .setTitle('Memento Mori')
-    .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
-    .setColor(randomColor)
-    .setImage(mementoMori.name)
-    .setDescription('[The Goodbye](https://www.youtube.com/watch?v=aDQ3nfBbPWM)');
-  message.channel.send({ embeds: [embed], files: [mementoMori] });
-  setTimeout(() => {
-    if (message.deletable) message.delete();
-  }, 1000);
+  public override async messageRun(message: Message) {
+    await message.delete().catch(() => null);
+    await message.author.send(await this.sharedRun(message));
+  }
 }
