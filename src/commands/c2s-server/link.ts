1edf2db9fa5e29847d04ebdccbf2e71ee2b30ab9
SirH <micahbenac52@gmail.com>
2022-04-30 07:24:33 -0400

    link slash command

--- src/commands/c2s-server/link.ts
+++ src/commands/c2s-server/link.ts
@@ -1,8 +1,8 @@
 import { c2sGuildId } from '#config';
 import { createHmac } from 'crypto';
-import type { Args } from '@sapphire/framework';
+import type { ApplicationCommandRegistry, Args } from '@sapphire/framework';
 import { Command } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import type { CommandInteraction, Message } from 'discord.js';
 import { Categories } from '#constants/index';
 
 // TODO: make this no longer require the need for stupid DMs
@@ -16,6 +16,41 @@ export default class Link extends Command {
         'link your C2S game progress with your Discord account for simplifying the process of gaining roles in the community.',
       fullCategory: [Categories.dm],
     });
+  }
+
+  public override async chatInputRun(interaction: CommandInteraction<'cached'>) {
+    const playerId = interaction.options.getString('playerId', true);
+    const playerToken = interaction.options.getString('playerToken', true);
+    const { user } = interaction;
+
+    const token = createHmac('sha1', process.env.USERDATA_AUTH).update(playerId).update(playerToken).digest('hex');
+    const dataAlreadyExists = await interaction.client.db.userData.findUnique({ where: { token } });
+    if (dataAlreadyExists)
+      return interaction.reply({
+        content:
+          'The provided data seems to already exist, which means this data is already linked to a discord account, if you feel this is false, please DM the owner(SirH).',
+        ephemeral: true,
+      });
+
+    await interaction.client.db.userData
+      .upsert({
+        where: { discord_id: user.id },
+        update: { token },
+        create: { discord_id: user.id, token },
+      })
+      .then(async () => {
+        console.log(`${user.tag}(${user.id}) successfully linked their C2S data.`);
+        await interaction.reply({
+          content: 'The link was successful, now you can use the Discord button in-game to upload your progress.',
+          ephemeral: true,
+        });
+      })
+      .catch(() =>
+        interaction.reply({
+          content: "An error occured, either you provided incorrect input or something randomly didn't want to work.",
+          ephemeral: true,
+        }),
+      );
   }
 
   public override async messageRun(message: Message, args: Args) {
@@ -68,5 +103,31 @@ export default class Link extends Command {
       );
 
     if (message.channel.type != 'DM') await message.delete().catch(() => null);
+  }
+
+  public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    registry.registerChatInputCommand(
+      {
+        name: this.name,
+        description: this.description,
+        options: [
+          {
+            name: 'playerId',
+            type: 'STRING',
+            description: 'Your in-game player Id.',
+            required: true,
+          },
+          {
+            name: 'playerToken',
+            type: 'STRING',
+            description: 'Your in-game player Token.',
+            required: true,
+          },
+        ],
+      },
+      {
+        guildIds: [c2sGuildId],
+      },
+    );
   }
 }
