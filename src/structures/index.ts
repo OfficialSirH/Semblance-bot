a7988188f868329e9d7183f0a72151a9c6606f71
SirH <micahbenac52@gmail.com>
2022-07-13 16:49:53 -0400

    forgor to remove export

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,4 +1,3 @@
 export { DBLApi } from '#structures/DiscordBotListAPI';
 export { DBotsApi } from '#structures/DiscordBotsAPI';
-export { DListApi } from '#structures/DiscordListAPI';
 export { DiscordsApi } from '#structures/DiscordsAPI';
