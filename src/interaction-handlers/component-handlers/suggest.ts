1cf7faddb956fd3617bf67643bda30a4b1dccf05
Micah Benac <micahbenac52@gmail.com>
2022-10-04 21:15:59 -0400

    fix(tweet): debug logs don't spam the same tweet

--- src/interaction-handlers/component-handlers/suggest.ts
+++ src/interaction-handlers/component-handlers/suggest.ts
@@ -18,7 +18,7 @@ export default class Suggest extends InteractionHandler {
     super(context, {
       ...options,
       name: 'suggest',
-      interactionHandlerType: InteractionHandlerTypes.MessageComponent,
+      interactionHandlerType: InteractionHandlerTypes.ModalSubmit,
     });
   }
 
